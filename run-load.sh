8582fb00d3172af144f1b24f8b68a46ed65f33e6
Daniel Edgar <aknot242@gmail.com>
2019-10-19 15:05:22 -0700

    increasing privs for docker execution

--- run-load.sh
+++ run-load.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
-docker run -it -e "HOST=$1" -v $(pwd):/tmp --entrypoint /bin/ash har-replay '/tmp/transform-har.sh'
+sudo docker run -it -e "HOST=$1" -v $(pwd):/tmp --entrypoint /bin/ash har-replay '/tmp/transform-har.sh'
 
 i=1
 while [[ $i -le $2 ]]
 do
     echo "Run iteration $i"
-    docker run -it -v $(pwd):/tmp aknot242/har-replay node main.js -f /tmp/output.har
+    sudo docker run -it -v $(pwd):/tmp aknot242/har-replay node main.js -f /tmp/output.har
     ((i = i + 1))
 done
