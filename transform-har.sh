ea4403c9a1506e0c0f67b1b0cb262cb1c2522a55
Daniel Edgar <aknot242@gmail.com>
2019-10-19 17:24:21 -0700

    fixing syntax issues

--- transform-har.sh
+++ transform-har.sh
@@ -1,13 +1,8 @@
-OUT_FILE=/tmp/output.har
-
 # this cli package can be used to pre-process HAR files that have handlebars variables in them
 npm list -g | grep handlebars-cmd || npm install -g handlebars-cmd
 
 # parse the hostname from the url
-HOST="$(awk -F/ '{print $3}' <<<$BASE_URL)"
-
-# remove output file if exists
-rm -f $OUT_FILE
+HOST="$(awk -F/ '{print $3}' << $BASE_URL)"
 
 # transform the HAR template with variables passed in
 cat /tmp/JuiceShop_Template.har | handlebars --base_url $BASE_URL --host $HOST > $OUT_FILE
