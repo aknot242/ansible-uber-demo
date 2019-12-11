1157269a7cd77af95341320c417ab9009734a450
Daniel Edgar <aknot242@gmail.com>
2019-12-10 18:06:08 -0800

    adding force option

--- install-centos-dependencies.sh
+++ install-centos-dependencies.sh
@@ -2,14 +2,11 @@
 
 # install dependencies
 sudo yum update
-sudo yum install git
-sudo yum install epel-release
+sudo yum install -y git wget epel-release ansible
 sudo subscription-manager repos --enable rhel-7-server-ansible-2.9-rpms
-sudo yum install ansible
 sudo pip install jmespath
 curl -fsSL https://get.docker.com/ | sh
 
 # install inspec
-sudo apt install wget
 wget https://packages.chef.io/files/stable/inspec/4.18.39/el/7/inspec-4.18.39-1.el7.x86_64.rpm
 sudo dpkg -i inspec-4.18.39-1.el7.x86_64.rpm
